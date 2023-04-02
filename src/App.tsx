580029f501d098e9717dfaf16feff4d16d8bb87a
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-04-02 19:38:39 +0900

    add gpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, TextField } from '@mui/material';
 import './App.css';
+import { useChatGpt } from './hooks/useChatGpt';
 
 function App() {
-  return <div className="App"></div>;
+  const [text, setText] = useState('');
+  const { fetchMessage, message } = useChatGpt();
+
+  return (
+    <div className="App">
+      <TextField
+        fullWidth
+        value={text}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setText(event.target.value);
+        }}
+      />
+      <Button fullWidth variant="contained" onClick={() => fetchMessage(text)}>
+        Submit
+      </Button>
+      <div>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
